ec26e12a3c407d6f5f90a24a1243602f1ae7a77f
ksr2code <ksr2code@gmail.com>
2026-01-16 00:29:17 +0100

    added ex3 main template

---
+++ ex3/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main_ex3():
+    print("=== DataDeck Game Engine ===")
+    print()
+    print("Configuring Fantasy Card Game...")
+
+
+if __name__ == "__main__":
+    main_ex3()
