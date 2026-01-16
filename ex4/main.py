8140c7b026a327fb7deef611c8404575974ae49f
ksr2code <ksr2code@gmail.com>
2026-01-17 00:33:11 +0100

    ex4 structure built

---
+++ ex4/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    try:
+        print("=== DataDeck Tournament Platform ===")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
