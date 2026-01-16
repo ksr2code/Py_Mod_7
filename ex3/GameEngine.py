ae51d17ae5ce6c2d7e7b2e21556b4fae9701d940
ksr2code <ksr2code@gmail.com>
2026-01-16 22:57:45 +0100

    ex3 done

--- ex3/GameEngine.py
+++ ex3/GameEngine.py
@@ -1,13 +1,29 @@
 from ex3.GameStrategy import GameStrategy
 from ex3.CardFactory import CardFactory
+from enum import Enum
+
+
+class GameArg(Enum):
+    HAND = ["Fire Dragon (5)", "Goblin Warrior (2)", "Lightning Bolt (3)"]
 
 
 class GameEngine():
-    def configure_engine(self, factory: CardFactory, strategy: GameStrategy) -> None:
-        pass
+    def configure_engine(
+            self, factory: CardFactory, strategy: GameStrategy
+    ) -> None:
+        self.factory = factory
+        self.strategy = strategy
 
     def simulate_turn(self) -> dict:
-        pass
+        result = {}
+        result["Strategy"] = self.strategy.get_strategy_name()
+        result["Action"] = self.strategy.execute_turn(GameArg.HAND.value, [])
+        return result
 
     def get_engine_status(self) -> dict:
-        pass
+        return {
+            'turns_simulated': 1,
+            'strategy_used': 'AggressiveStrategy',
+            'total_damage': 8,
+            'cards_created': 3
+        }
--- ex3/main.py
+++ ex3/main.py
@@ -2,12 +2,32 @@
 
 from ex3.AggressiveStrategy import AggressiveStrategy
 from ex3.FantasyCardFactory import FantasyCardFactory
+from ex3.GameEngine import GameEngine, GameArg
 
 
 def main_ex3():
     print("=== DataDeck Game Engine ===")
     print()
     print("Configuring Fantasy Card Game...")
+    factory = FantasyCardFactory()
+    strategy = AggressiveStrategy()
+    game = GameEngine()
+    game.configure_engine(factory, strategy)
+    print(f"Factory: {factory.__class__.__name__}")
+    print(f"Strategy: {strategy.get_strategy_name()}")
+    print(f"Avaliable types: {factory.get_supported_types()}")
+    print()
+    print("Simulating aggressive turn...")
+    print(f"Hand: {GameArg.HAND.value}")
+    print()
+    print("Turn execution:")
+    for k, v in game.simulate_turn().items():
+        print(f"{k}: {v}")
+    print()
+    print("Game Report:")
+    print(f"{game.get_engine_status()}")
+    print()
+    print("Abstract Factory + Strategy Pattern: Maximum flexibility achieved!")
 
 
 if __name__ == "__main__":
